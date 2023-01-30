381dc37862e6d27d5ff3fb901708d54c625d8bc7
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-01-30 17:24:22 +0100

    first commit

---
+++ My Great App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Great App
+//
+//  Created by Stewart Lynch on 2021-09-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Great App/My_Great_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Great_AppApp.swift
+//  My Great App
+//
+//  Created by Stewart Lynch on 2021-09-21.
+//
+
+import SwiftUI
+
+@main
+struct My_Great_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
